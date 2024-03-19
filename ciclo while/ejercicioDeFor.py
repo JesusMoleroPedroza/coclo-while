70a8b893be51b61a6e327d4baae175b07dde1395
JesusMoleroPedroza <160894545+JesusMoleroPedroza@users.noreply.github.com>
2024-03-19 11:49:41 -0500

    ciclo while

---
+++ ciclo while/ejercicioDeFor.py
@@ -0,0 +1,10 @@
+vuelo = {
+    'Aerolinea':'Avianca',
+    'vuelo':'AV3102',
+    'origen':'CTG',
+    'Destino':'MDE',
+    'tipo_Maleta':['Cabina','Mano','Bodega']
+}
+
+for key,value in vuelo:
+    print(key,':',value)
---
+++ ciclo while/ejercicioDeWhile.py
@@ -0,0 +1,15 @@
+x=0
+while (x < 5):
+    print("1.Personas \n2.Vehiculos \n3.Universidades \n4.Notas \n5.Salir")
+    x = int(input("escoga una opción: "))
+    
+    if (x==1):
+        print("Hola has presionado la opcion personas.")
+    elif (x==2):
+        print("Hola has presionado la opcion vehiculos.")
+    elif (x==3):
+        print("Hola has presionado la opcion universidades.")
+    elif (x==4):
+        print("Hola has presionado la opcion notas.")
+
+print("hola has presionado la opcion salir gracias por usar nuestro programa.")
