df9547096ef562816e6b17fbb83bcdf99e416806
JesusMoleroPedroza <160894545+JesusMoleroPedroza@users.noreply.github.com>
2024-03-20 09:56:59 -0500

    ejercicioDeFor.py

--- ciclo while/ejercicioDeFor.py
+++ ciclo while/ejercicioDeFor.py
@@ -1,10 +1,16 @@
-vuelo = {
-    'Aerolinea':'Avianca',
-    'vuelo':'AV3102',
-    'origen':'CTG',
-    'Destino':'MDE',
-    'tipo_Maleta':['Cabina','Mano','Bodega']
+Vuelo = {
+    'Aerolinea': 'Avianca',
+    'Vuelo': 'AV3102',
+    'Origen': 'CTG',
+    'Destino': 'MDE',
+    'Tipo_Maleta': ['Cabina', 'Mano', 'Bodega']
 }
 
-for key,value in vuelo:
-    print(key,':',value)
+
+print("Los valores del diccionario Vuelo son:")
+for key, value in Vuelo.items():
+    print(key + ":", value)
+print("\n")
+print("Valores de tipo de maleta:")
+for tipo_maleta in Vuelo['Tipo_Maleta']:
+    print(tipo_maleta)
